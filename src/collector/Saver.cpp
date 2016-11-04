8efd40626622019d0f5da678a84a850010f6a990
Roberto Calvo <rocapal@gmail.com>
2016-11-04 23:52:21 +0100

    refactoring about the single storage file

--- src/collector/Saver.cpp
+++ src/collector/Saver.cpp
@@ -62,8 +62,10 @@ void Saver::run ()
 				//Normalize
 				cvec capbuf;
 				capbuf.set_size(samples.size()/2);
-				for (unsigned int t=0;t<samples.size();t=t+2) {
+				// TODO: Be sure that size() is a even
+				for (unsigned int t=0;t<samples.size()-1;t=t+2) {
 					capbuf(t/2)=std::complex<double>((((double)samples[t])-127.0)/128.0,(((double)samples[(t+1)])-127.0)/128.0);
+					//capbuf(t/2)=std::complex<double>( (double)samples[t] , (double)samples[(t+1)]);
 					//if (t==20)
 					//	std::cout << capbuf(t/2) << std::endl;
 				}
@@ -75,15 +77,30 @@ void Saver::run ()
 						mFileName << "/tmp" << "/capbuf_" << segment->sensorID << "_" << segment->timestamp->sec << "." << segment->timestamp->nsec << ".it";
 						// Save file
 						mItFile.open(mFileName.str(),true);
-						mFinalBuffer = concat(mFinalBuffer, capbuf);
+						//mFinalBuffer = concat(mFinalBuffer, capbuf);
 
+						mItFile << Name("capbuf") << capbuf;
+						mItFile.close();
+						capbuf.clear();
 					}
 					else
 					{
 						// We save data in memory until the end.
 						m_fc_v(0)=segment->centerFrequency;
+
+						mFinalBuffer.clear();
+						mItFile.open(mFileName.str(), false);
+						mItFile >> Name("capbuf") >> mFinalBuffer;
 						mFinalBuffer = concat(mFinalBuffer, capbuf);
+						mItFile.close();
+
+						mItFile.open(mFileName.str(),true);
+						mItFile << Name("capbuf") << mFinalBuffer;
+						mItFile << Name("fc") << m_fc_v;
+						mItFile.close();
+
 						capbuf.clear();
+
 					}
 				}
 				else
@@ -101,6 +118,7 @@ void Saver::run ()
 					mItFile.close();
 					capbuf.clear();
 				}
+
 			}
 
 		}
@@ -110,12 +128,12 @@ void Saver::run ()
 
 void Saver::finish()
 {
-	if (mUniqueFile)
+/*	if (mUniqueFile)
 	{
-		mItFile << Name("capbuf") << mFinalBuffer;
 		mItFile << Name("fc") << m_fc_v;
 		mItFile.close();
 	}
+	*/
 }
 
 Saver::~Saver() {
